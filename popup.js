1117cd54f61c37656bedadf37aaecce7de49be1c
Chris Palmer <chris@noncombatant.org>
2025-01-04 11:29:10 -0800

    Add the Enabled checkbox
    To allow people to turn filtering on/off easily.

--- popup.js
+++ popup.js
@@ -28,32 +28,34 @@ const removeRuleIds = [...Array(chrome.declarativeNetRequest.MAX_NUMBER_OF_DYNAM
 
 export const applyBlorpList = async function (block) {
   const options = { addRules: [], removeRuleIds: removeRuleIds }
-  const patterns = localStorage.getItem("blorpList").split(/[\r\n]+/).map(p => p.trim()).filter(p => p.length > 0)
-  let i = 0
-  for (; i < patterns.length; i++) {
-    const pattern = patterns[i]
-    options.addRules.push({
-      id: i + 1,
-      priority: 1,
-      action: { type: block ? "block" : "allow" },
-      condition: {
-        resourceTypes: resourceTypes,
-        urlFilter: pattern,
-        isUrlFilterCaseSensitive: false,
-      }
-    })
-  }
-  if (!block) {
-    options.addRules.push({
-      id: i + 1,
-      priority: 1,
-      action: { type: "block" },
-      condition: {
-        resourceTypes: resourceTypes,
-        urlFilter: "*://*",
-        isUrlFilterCaseSensitive: false,
-      }
-    })
+  if (listEnabled.checked) {
+    const patterns = localStorage.getItem("blorpList").split(/[\r\n]+/).map(p => p.trim()).filter(p => p.length > 0)
+    let i = 0
+    for (; i < patterns.length; i++) {
+      const pattern = patterns[i]
+      options.addRules.push({
+        id: i + 1,
+        priority: 1,
+        action: { type: block ? "block" : "allow" },
+        condition: {
+          resourceTypes: resourceTypes,
+          urlFilter: pattern,
+          isUrlFilterCaseSensitive: false,
+        }
+      })
+    }
+    if (!block) {
+      options.addRules.push({
+        id: i + 1,
+        priority: 1,
+        action: { type: "block" },
+        condition: {
+          resourceTypes: resourceTypes,
+          urlFilter: "*://*",
+          isUrlFilterCaseSensitive: false,
+        }
+      })
+    }
   }
   await chrome.declarativeNetRequest.updateDynamicRules(options)
   log("Loaded", options.addRules.length, "rules")
@@ -61,12 +63,15 @@ export const applyBlorpList = async function (block) {
 
 const onApplyButtonClick = async function (event) {
   localStorage.setItem("blorpList", blorpList.value)
+  localStorage.setItem("listEnabled", listEnabled.checked ? "true" : "false")
   localStorage.setItem("listType", listTypeBlock.checked ? "block" : "allow")
   await applyBlorpList(listTypeBlock.checked)
 }
 
 document.body.onload = async function (event) {
   blorpList.value = localStorage.getItem("blorpList")
+  let enabled = localStorage.getItem("listEnabled")
+  listEnabled.checked = enabled !== "false"
   let listType = localStorage.getItem("listType")
   listTypeBlock.checked = listType !== "allow"
   listTypeAllow.checked = listType === "allow"
